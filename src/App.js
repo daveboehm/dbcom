243e8cfb47dc5cca129e0d0ec58a6c483c7c84e3
dave boehm <davidnboehm@gmail.com>
2021-02-07 12:08:44 -0600

    testing cicd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Dave Boehm</h1>
+        <h1>DBCOM</h1>
         <AmplifySignOut />
       </header>
     </div>
